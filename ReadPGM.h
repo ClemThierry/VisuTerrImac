f9e80f069e32992ab6afe03483dbe771605743c1
ClemThierry <49915513+ClemThierry@users.noreply.github.com>
2021-05-21 12:03:23 +0200

    Lecture des infos de l'image pbm

---
+++ ReadPGM.h
@@ -0,0 +1,18 @@
+#ifndef READPGM_H
+#define READPGM_H
+
+// Structure for storing the
+// image data
+typedef struct PGMImage {
+	char pgmType[3];
+	unsigned char** data;
+	unsigned int width;
+	unsigned int height;
+	unsigned int maxValue;
+} PGMImage;
+
+void ignoreComments(FILE* fp);
+bool openPGM(PGMImage* pgm, const char* filename);
+void printImageDetails(PGMImage* pgm, const char* filename);
+
+#endif
